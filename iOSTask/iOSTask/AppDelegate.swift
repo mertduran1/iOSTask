38b2e68f0135abcaf9d1d5ff022ede1d7dc3ba82
Mert Duran <mrtdrn4@icloud.com>
2023-02-10 10:31:11 +0300

    created project

---
+++ iOSTask/iOSTask/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iOSTask
+//
+//  Created by Mert Duran on 10.02.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .cyan
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+    
+}
+
